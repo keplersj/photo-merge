42524e60134ffe747cf3f7b3ebda1e62c22b1d91
Kepler Sticka-Jones <kepler@stickajones.org>
2024-01-03 01:29:08 -0700

    blender

--- app.py
+++ app.py
@@ -7,16 +7,22 @@ processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-larg
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 
-file_name = st.file_uploader("Upload image 1")
+files = st.file_uploader("Upload images to blend", accept_multiple_files=True)
+descs = []
 
-if file_name is not None:
+for file_name in files:
     image = Image.open(file_name)
+    st.image(image)
 
     inputs = processor(image, return_tensors="pt")
 
     out = model.generate(**inputs)
     description = processor.decode(out[0], skip_special_tokens=True)
+    descs.append(description)
     st.write(description)
-    
-    image = pipe(description).images[0]
-    st.image(image)
+
+if descs.count() > 0:
+    description = ' '.join(descs)
+    st.write(description)
+    for image in pipe(description):
+        st.image(image)
