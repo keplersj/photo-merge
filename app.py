4e343fc4068c60b1f46adee655315063bb7514dd
Kepler Sticka-Jones <kepler@stickajones.org>
2024-01-02 23:34:09 -0700

    diffuse?

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
+from diffusers import StableDiffusionPipeline
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 
 file_name = st.file_uploader("Upload image 1")
 
@@ -13,4 +15,8 @@ if file_name is not None:
     inputs = processor(image, return_tensors="pt")
 
     out = model.generate(**inputs)
-    st.write(processor.decode(out[0], skip_special_tokens=True))
+    description = processor.decode(out[0], skip_special_tokens=True)
+    st.write(description)
+    
+    image = pipe(description).images[0]
+    st.image(image)
