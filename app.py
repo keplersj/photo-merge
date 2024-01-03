3558088f7d0cc118a21e941fb5a93dcc65a4aba9
Kepler Sticka-Jones <kepler@stickajones.org>
2024-01-02 23:12:13 -0700

    Blip

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+file_name = st.file_uploader("Upload image 1")
+
+image = Image.open(file_name)
+
+inputs = processor(image, return_tensors="pt")
+
+out = model.generate(**inputs)
+st.write(processor.decode(out[0], skip_special_tokens=True))
