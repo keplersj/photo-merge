4dba69ffa386a255a41c7b6b6b9f90ae3b1aa9ab
Kepler Sticka-Jones <kepler@stickajones.org>
2024-01-03 21:51:27 -0700

    use transfromer pipeline

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import streamlit as st
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import pipeline as transformer
 from diffusers import StableDiffusionPipeline
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 
 captions = []
 
 with st.sidebar:
     files = st.file_uploader("Upload images to blend", accept_multiple_files=True)
+    st.divider()
+    caption_model = st.selectbox("Caption Model", [
+        "ydshieh/vit-gpt2-coco-en",
+        "Salesforce/blip-image-captioning-large",
+        "nlpconnect/vit-gpt2-image-captioning",
+        "microsoft/git-base"
+    ])
     st.divider()
     image_gen_guidance = st.slider("Stable Diffusion: Guidance Scale", value=7.5)
     image_gen_steps = st.slider("stable Diffusion: Inference Steps", value=50)
@@ -22,12 +27,11 @@ with col1:
         image = Image.open(file_name)
 
         with st.spinner('Captioning Provided Image'):
-            inputs = processor(image, return_tensors="pt")
-            out = model.generate(**inputs)
-            description = processor.decode(out[0], skip_special_tokens=True)
-            captions.append(description)
+            captioner = transformer(model=caption_model)
+            caption = captioner(image)[0].generated_text
 
-        st.image(image, caption=description)
+        captions.append(caption)
+        st.image(image, caption=caption)
 
 with col2:
     if len(captions) > 0:
